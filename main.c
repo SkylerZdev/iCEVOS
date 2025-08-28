fb70f251e88f64dbc4d833897c4e64210de01e24
ZpexLakip <ericsmoura01@gmail.com>
2025-08-28 06:51:23 -0300

    Add files via upload
    Inicio do projeto com a criação do menu principal com as opções:
    - Acessar o escalonador de processos
    - Encerrar o programa
    
    No printf do escalonador, foram adicionadas as seguintes funcionalidades:
    1. Carregar processos do arquivo (função carregarProcessos)
    2. Executar ciclo único (função executarCiclo)
    3. Executar até terminar (função rodarEscalonador)
    4. Retornar ao menu principal
    
    Também foi incluí respostas para :
    - Validação de opções inválidas.
    
    Próximos passos:
    - Criação da Fila para o Escalonador.
    - Analisar pontos de melhora do código

---
+++ main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "MyLib.h"
+
+int opMenuPrincipal, opMenuEscalonador;
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    do {
+        system("cls");
+        printf("|====== MENU PRINCIPAL ======|\n");
+        printf("|1 - Escalonador de Processos|\n");
+        printf("|         2 - Sair           |\n");
+        printf("|==========================  |\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opMenuPrincipal);
+
+        switch(opMenuPrincipal) {
+            case 1: {
+                do {
+                    system("cls");
+                    printf("|========= ESCALONADOR ===========|\n");
+                    printf("|1 - Carregar processos do arquivo|\n");
+                    printf("|2 - Executar ciclo único         |\n");
+                    printf("|3 - Executar até terminar        |\n");
+                    printf("|4 - Voltar ao menu principal     |\n");
+					printf("|=================================|\n");
+                    printf("Escolha: ");
+                    scanf("%d", &opMenuEscalonador);
+
+                    switch(opMenuEscalonador) {
+                        case 1:
+                            carregarProcessos();
+                            system("pause");
+                            break;
+                        case 2:
+                            if (todasListasVazias()) {
+                                printf("Nenhum processo carregado!\n");
+                            } else {
+                                executarCiclo();
+                            }
+                            system("pause");
+                            break;
+                        case 3:
+                            rodarEscalonador();
+                            system("pause");
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            printf("Opção inválida!\n");
+                            system("pause");
+                            break;
+                    }
+                } while (opMenuEscalonador != 4);
+                break;
+            }
+            case 2:
+                printf("Sair\n");
+                break;
+            default:
+                printf("Opção inválida!\n");
+                system("pause");
+                break;
+        }
+    } while (opMenuPrincipal != 2);
+
+    return 0;
+}
