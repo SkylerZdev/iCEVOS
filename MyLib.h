cb6dcab21d82107af434cab611f099ad30e10ee3
ZpexLakip <ericsmoura01@gmail.com>
2025-08-29 12:28:54 -0300

    Update MyLib.h
    Leitura do Arquivo Não usa mais fscanf, agora, fgets.
    Adição da função Char em arquivo para definir o tamanho de caracteres que pode ser lido por linha

--- MyLib.h
+++ MyLib.h
@@ -99,24 +99,26 @@ Node *Get(Fila *f, int i){ //Retorna o nó da posição i da fila
 }
 
 void carregarProcessos() {  //Parte da Leitura de Arquivo
-    FILE *f = fopen("Processos/processos","r");
+    FILE *f = fopen("Processos/processos","r"); //Abre o Diretoório do arquivo 
     if (!f) { printf("Erro ao abrir processos.txt\n"); return 0; }
 
     int id, pri, cic; char nome[40], recurso[12];
-    while (fscanf(f,"%d %39s %d %d %11s", &id,nome,&pri,&cic,recurso)==5) { //Não seria melhor um Fgets?
-        Processo *p;
-        p->id=id;
-        strcpy(p->nome,nome);
-        p->prioridade=(pri<1||pri>3)?3:pri;
-        p->ciclos=cic;
-        if (strcmp(recurso,"-")==0) recurso[0]='\0';
-        strcpy(p->recurso,recurso);
-        p->prioridadeOriginal=p->prioridade;
-        p->jaBloqueado=false;        
-
-        if (p->prioridade==1) fila_adicionar(&escalonador.Alta,p);
-        else if (p->prioridade==2) fila_adicionar(&escalonador.Media,p);
-        else fila_adicionar(&escalonador.Baixa,p);
+    char linha[128]; //Tamanho de caracteres que pode ser lido por linha
+    while (fgets(linha, sizeof(linha), f)) { //Lê linha por linha do arquivo, melhor que fscanf
+        if (sscanf(linha, "%d %39s %d %d %11s", &id, nome, &pri, &cic, recurso) == 5) {
+            Processo p;
+            p.id = id;
+            strcpy(p.nome, nome);
+            p.prioridade = (pri < 1 || pri > 3) ? 3 : pri;
+            p.ciclos = cic;
+            if (strcmp(recurso, "-") == 0) recurso[0] = '\0';
+            strcpy(p.recurso, recurso);
+            p.prioridadeOriginal = p.prioridade;
+            p.jaBloqueado = false;
+            if (p.prioridade == 1) fila_adicionar(&escalonador.Alta, &p);
+            else if (p.prioridade == 2) fila_adicionar(&escalonador.Media, &p);
+            else fila_adicionar(&escalonador.Baixa, &p);
+        }
     }
     fclose(f);
     printf("Processos carregados com sucesso!\n");
